c9dc37def9fcb9216e370bf6325cef3270efa0bd
Abdulloh <abdullohabduhamidov@gmail.com>
2022-11-15 17:41:37 +0500

    js

---
+++ total.js
@@ -0,0 +1,31 @@
+alert("welcome");
+var name = prompt("name");
+var ssss = `salom ${name}`;
+console.log(ssss);
+var surname = prompt("surname");
+var watisyourname = `salom ${surname}`;
+console.log(watisyourname);
+
+var emall = prompt("emall");
+var emall2 = `your ${emall}`;
+console.log(emall2);
+var all = alert(
+  `your name : ${name} \n your surname : ${surname} \n your emall ${emall}`
+);
+var a = 5;
+var b = 10;
+console.log(a + b);
+var a = 25;
+var b = 15;
+var result = a - b;
+console.log(result);
+var a = 10;
+var b = 5;
+var c = a - b;
+console.log(c);
+var a = 10;
+var b = 3;
+console.log(a % b);
+var a = 2;
+var b = 10;
+console.log(a ** b);
